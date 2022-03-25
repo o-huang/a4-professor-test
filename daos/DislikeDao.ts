d0d2ab62a0ab85ced551cdb0453284c5666b8e10
owen huang <ohuang2@gmail.com>
2022-03-25 18:57:00 -0400

    Fix another misspelled word

--- daos/DislikeDao.ts
+++ daos/DislikeDao.ts
@@ -1,6 +1,6 @@
 import DislikeDaoI from "../interfaces/DislikeDaoI";
 import DislikeModel from "../mongoose/dislikes/DislikeModel";
-import Dislike from "../models/dislikes/dislike";
+import Dislike from "../models/dislikes/Dislike";
 import LikeModel from "../mongoose/likes/LikeModel";
 export default class DislikeDao implements DislikeDaoI {
 
