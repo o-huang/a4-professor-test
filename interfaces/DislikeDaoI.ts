1d1f0ae3d320687a54a4d8b2f3b87ace02bfc070
owen huang <ohuang2@gmail.com>
2022-03-25 17:21:03 -0400

    Create dislike dao interface

---
+++ interfaces/DislikeDaoI.ts
@@ -0,0 +1,9 @@
+import Dislike from "../models/dislikes/Dislike";
+
+/**
+ * @file Declares API for Dislikes related data access object methods
+ */
+export default interface DislikeDaoI {
+    findAllUsersThatDislikedTuit (tid: string): Promise<Dislike[]>;
+    findAllTuitsDislikedByUser (uid: string): Promise<Dislike[]>;
+};
